7951f8ca54da280117f6a2320df3736477bdc059
Davner Firmino Cavalcante <davner23@gmail.com>
2022-07-30 03:27:04 -0400

    📃Docs: Instalação do ReactJS com Vite + TailwindCSS - 23/07/2022

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  
+
+  return (
+    <div className="text-center text-5xl bg-gray-700">
+      <h1 className="text-white">Olá mundo!!</h1>
+    </div>
+  );
+}
+
+export default App;
